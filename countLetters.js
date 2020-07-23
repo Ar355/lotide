e36e39f09339fb23bbf4b696dc9b683866dcd3df
Ar355 <zeneli.arian@gmail.com>
2020-07-23 04:26:14 +0000

    commiting the solution for w1d3 countLetters

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️✔️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑 Assertion Failed: ${actual}!== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  console.log(string);
+  const objectCount = {};
+  for (let indx of string) {
+    if (objectCount[indx]) {
+      objectCount[indx]++;
+    } else {
+      objectCount[indx] = 1;
+    }
+    
+  }
+  return objectCount;
+};
+
+assertEqual(countLetters('lhl'), {l: 2, h: 1});
